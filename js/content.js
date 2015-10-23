84afcfbfe1fdbc408278da017747c2cb66122e94
Milos Kovacevic <miloscpt@gmail.com>
2015-10-24 00:18:26 +0200

    Small changes on content.js

--- js/content.js
+++ js/content.js
@@ -3,17 +3,19 @@ chrome.extension.onMessage.addListener(
         debugger;
 
         if(request.action == "Replace"){
-            var load = "SUCCESS";
-            var elements = document.getElementsByTagName("img");
-            var anchors = document.getElementsByTagName("a");
-            var replacementHref = "http://usatlife.files.wordpress.com/2014/06/terrycrewspecs_take2.gif?w=1000";
+            setInterval(function(){
+                var elements = document.getElementsByTagName("img");
+                var anchors = document.getElementsByTagName("a");
+                var replacementHref = "http://usatlife.files.wordpress.com/2014/06/terrycrewspecs_take2.gif?w=1000";
 
-            for(var i = 0; i < elements.length; i++){
-                elements[i].src = replacementHref;
-            }
-            for(var i = 0; i < anchors.length; i++){
-                anchors[i].href = replacementHref;
-            }
+                for(var i = 0; i < elements.length; i++){
+                    elements[i].src = replacementHref;
+                }
+                for(var i = 0; i < anchors.length; i++){
+                    anchors[i].href = replacementHref;
+                }
+            }, 3000);
+
             sendResponse(load);
         }
     }
